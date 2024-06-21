9f759bc7cbb17f00655b90f0fd7d732b0d00fd03
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-06-21 13:42:40 +0700

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -1,19 +1,17 @@
 import mysql.connector
-from mysql.connector import Error
-import os
+import pandas as pd
 
 def create_connection():
-    try:
-        connection = mysql.connector.connect(
-            host=os.getenv('DB_HOST', 'localhost'),  # Replace with your actual host
-            user=os.getenv('DB_USER', 'root'),      # Replace with your MySQL username
-            password=os.getenv('DB_PASSWORD', ''),  # Replace with your MySQL password
-            database=os.getenv('DB_NAME', 'aw')     # Replace with your MySQL database name
-        )
-        if connection.is_connected():
-            print("Successfully connected to the database")
-            connection.close()
-    except Error as err:
-        print(f"Error: {err}")
+    return mysql.connector.connect(
+        host="localhost",         
+        user="root",            
+        password="",      
+        database="aw"  
+    
+    )
 
-create_connection()
+def get_data_from_db(query):
+    conn = create_connection()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
